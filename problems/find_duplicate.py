ab04143d6fa2573e04ba80571e4f17aac6efecd2
Bogomil33 <radu01binhn@yahoo.com>
2024-10-22 16:33:15 +0300

    pep8 compliant

--- problems/find_duplicate.py
+++ problems/find_duplicate.py
@@ -1,7 +1,7 @@
 import os
 
-duplicateList = []
-allStuff = []
+duplicate_list = []
+all_stuff = []
 def compare_files(file1,  file2):
     with open(file1, 'r', errors="replace") as f1, open(file2, 'r', errors="replace") as f2:
         content1 = f1.read()
@@ -9,24 +9,24 @@ def compare_files(file1,  file2):
 
     if content1 == content2:
         if file1 != file2:
-            if file1 not in duplicateList:
-                duplicateList.append(file1)
-            if file2 not in duplicateList:
-                duplicateList.append(file2)
+            if file1 not in duplicate_list:
+                duplicate_list.append(file1)
+            if file2 not in duplicate_list:
+                duplicate_list.append(file2)
 
 def search_all_files(directory):
     for dirpath, dirnames, filenames in os.walk(directory):
         for filename in filenames:
-            allStuff.append(os.path.join(dirpath, filename))
+            all_stuff.append(os.path.join(dirpath, filename))
 
-    for fileOriginal in allStuff:
-        for fileDuplicate in allStuff:
-            compare_files(fileOriginal, fileDuplicate)
+    for file_original in all_stuff:
+        for file_duplicate in all_stuff:
+            compare_files(file_original, file_duplicate)
 
 # Example usage
 search_all_files("C:/Users/radu0/OneDrive/Desktop/find_duplicate/")
 
-print(allStuff)
+print(all_stuff)
 
-print("duplicates:")
-print(duplicateList)
+print(f"duplicates:")
+print(duplicate_list)
